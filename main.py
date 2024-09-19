3a0e5295a7f28c9c86e86d99db924b2ad812f9e2
shiv2130 <shivanahuomili@gmail.com>
2024-09-19 09:14:41 +0530

    Plotted a designed graph

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import matplotlib.pyplot as plt 
 x = [1, 2, 3, 4, 5]
 y = [1, 2, 3, 4, 5]
-plt.plot(x, y)
+plt.plot(x, y, color = "green", linestyle = "dashed", linewidth = 3, marker = 'o', markerfacecolor = "blue", markersize = 12)
+plt.xlabel("x-axis")
+plt.ylabel("y-axis")
+plt.title("My first graph")
 plt.show()
