cc94304d74c8283a598a83a495cc8aa8fca36bdb
shiv2130 <shivanahuomili@gmail.com>
2024-09-23 16:10:17 +0530

    just added some details

--- histo.py
+++ histo.py
@@ -1,9 +1,11 @@
 import matplotlib.pyplot as plt 
+import numpy as np
 ages = [2, 5, 70, 30, 45, 50, 45, 43, 40, 44, 60, 7, 13, 57, 18, 90, 77, 32, 21, 20, 40]
 range = (0, 100)
 bins = 10 
-plt.hist(ages, bins, range)
+plt.hist(ages, bins, range, edgecolor = "Black")
 plt.xlabel("Age")
 plt.ylabel("No. of people")
 plt.title("My Histogram")
+
 plt.show()
