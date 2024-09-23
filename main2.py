79b0d7142345be75cdc14dfc1490d5245d277a44
shiv2130 <shivanahuomili@gmail.com>
2024-09-23 15:30:34 +0530

    Plotted bar graphs

--- main2.py
+++ main2.py
@@ -1,3 +1,6 @@
-import matplotlib.pyplot as plt 
-x1 = [1, 2, 3, 4]
-x2 = []
+import matplotlib.pyplot as plt
+import numpy as np 
+y = [1, 2, 3, 4]
+abc = ["one", "two", "three", "four"]
+plt.bar(np.arange(len(y)), y, tick_label = abc, width=0.4)
+plt.show()
