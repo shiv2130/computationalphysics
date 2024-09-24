b5fb104d18b414220cef4e52a45e3a4c09d5dafc
shiv2130 <shivanahuomili@gmail.com>
2024-09-24 09:10:41 +0530

    NO changes

--- pno.py
+++ pno.py
@@ -1,8 +1,10 @@
 import matplotlib.pyplot as plt 
+import numpy as np
 activities = ['eat', 'sleep', 'work', 'play']
 slices = [3, 7, 8, 6]
 colors = ['r', 'y', 'g', 'b']
 plt.pie(slices, labels = activities, colors = colors)
+plt.xticks()
 plt.show()
     
     
