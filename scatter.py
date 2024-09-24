1d4e649aa10f39ea3dfe2a2ee795c7e7d3bb6ccd
shiv2130 <shivanahuomili@gmail.com>
2024-09-24 09:24:00 +0530

    New scatter

--- scatter.py
+++ scatter.py
@@ -1,10 +1,11 @@
 import matplotlib.pyplot as plt
 import numpy as np 
 x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-y = [2, 4, 5, 7, 8, 9, 11, 13, 29, 20]
+y = [2, 4, 5, 7, 6, 7, 8, 9, 11, 13,]
 plt.scatter(x, y)
 plt.xlabel("X-axis")
 plt.ylabel("Y-axis")
 plt.title("SCATTER PLOT")
-plt.xticks(np.arange(min(x), max(x), 1.0))
+plt.xticks(np.arange(min(x), max(x)+1, 1.0))
+plt.yticks(np.arange(min(y), max(y)+1, 1.0))
 plt.show()
