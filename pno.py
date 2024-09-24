6e22924252c10f8abee545536c991d642afd394b
shiv2130 <shivanahuomili@gmail.com>
2024-09-24 08:39:19 +0530

    Pie chart

---
+++ pno.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt 
+activities = ['eat', 'sleep', 'work', 'play']
+slices = [3, 7, 8, 6]
+colors = ['r', 'y', 'g', 'b']
+plt.pie(slices, labels = activities, colors = colors)
+plt.show()
+    
+    
+    
+
+
+    
+
+
+
