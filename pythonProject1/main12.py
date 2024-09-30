e053e07849a7d3244468b94e9834cd490c4e3ad2
shiv2130 <shivanahuomili@gmail.com>
2024-09-30 12:23:19 +0530

    Just some operator precidency and associativity

--- pythonProject1/main12.py
+++ pythonProject1/main12.py
@@ -57,15 +57,68 @@ print(e)
 # a = a - b
 # print("After exchange: ")
 # print(a, b)
-a = 10
-b = 15
-c = 20
-w = a>b and b<a
-print(w)
-x = a<b and b<c
-print(x)
-y = a>b or b>c
-print(y)
-z = a<b or b<c
-print(z)
-print(not a)
+# a = 10
+# b = 15
+# c = 20
+# w = a>b and b<a
+# print(w)
+# x = a<b and b<c
+# print(x)
+# y = a>b or b>c
+# print(y)
+# z = a<b or b<c
+# print(z)
+# print(not a)
+# #bitwise operation
+# a = 29
+# b = 17
+# c = a&b
+# print(c)
+# d = a|b
+# print(d)
+# e = 10
+# f = 15
+# g = e^f
+# print(g)
+# x = 8
+# y = 6
+# z = x^y
+# print(z)
+# print(~e)
+# a = 10
+# b = a>>2
+# print(b)
+# c = 32
+# d = c>>3
+# print(d)
+# e = 32
+# f = e<<2
+# print(f)
+# s = " VIT BHOPAL UNIVERSITY"
+# print("A" in s)
+# print("vt" in s)
+# print("vt " not in s)
+# a = 10
+# c = ~a
+# print(c)
+a  =  "Hello World"
+b = a
+str1 = id(a)
+str2 = id(b)
+print(a is b)
+print(str1, str2)
+print(a, b)
+print(a == b)
+
+c = 5
+d = 6
+print(c is d)
+print(id(c), id(d))
+print(c == d)
+print(0.1+0.2 == 0.3)#-> false
+print(4*4/3+5-2)
+g = "Hello World"
+f = g[::-1]
+print(f)
+print(3*2**3) #**->Higher order priority and *->least order priority
+print(2**2**3)
