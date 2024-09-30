35a121bf60375b6a6baae6d54553ded4f2c550bc
shiv2130 <shivanahuomili@gmail.com>
2024-09-30 12:41:54 +0530

    basic programs in python

---
+++ pythonProject1/main32.py
@@ -0,0 +1,34 @@
+# #PROGRAM FOR CONVERTING CELSIUS TO FARENHEIT
+# f = float(input(" Enter temperature in celsius "))
+# c = ((f-32)/9)
+# print("The temperature in fahrenheit is : ", c)
+#
+# #program for simple interest
+# p = float(input("Enter principal: "))
+# r = float(input(" Enter rate of interest: "))
+# t = float(input("Enter time: "))
+# si =(p*r*t)/100
+# print("The simple interest: ", si)
+
+
+# #Program for swapping 2 values with 3rd variable
+# a = int(input("Enter a number: "))
+# b = int(input("Enter another number: "))
+# print("Value before exchange: ", a, b)
+# c = a
+# a = b
+# b = c
+# print("The exchange value: ", a, b)
+#
+# #Program for swapping 2 variables without 3rd variable
+
+
+#Largest number among two given input number
+a = int(input("Enter a number: "))
+b = int(input("Enter another number: "))
+if(a>b):
+    print(a, " is greater than ", b )
+else:
+    print(b, " is greater than ", a)
+
+
