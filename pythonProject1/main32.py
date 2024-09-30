021259c170a0f29d99740f27ff0d913859328ed3
shiv2130 <shivanahuomili@gmail.com>
2024-09-30 12:55:57 +0530

    "Another basic program for checking even odd"

--- pythonProject1/main32.py
+++ pythonProject1/main32.py
@@ -23,12 +23,31 @@
 # #Program for swapping 2 variables without 3rd variable
 
 
-#Largest number among two given input number
-a = int(input("Enter a number: "))
-b = int(input("Enter another number: "))
-if(a>b):
-    print(a, " is greater than ", b )
+# #Largest number among two given input number
+# a = int(input("Enter a number: "))
+# b = int(input("Enter another number: "))
+# if(a>b):
+#     print(a, " is greater than ", b )
+# else:
+#     print(b, " is greater than ", a)
+
+#largest number among 3 given numbers
+# a = int(input("Enter a number: "))
+# b = int(input("Enter another number: "))
+# c = int(input("Enter another number: "))
+# if(a>b and a>c):
+#     print(a, " is greater")
+# elif(b>a and b>c):
+#     print(b, "is greater")
+# else:
+#     print(c, "is greater")
+
+#Given input is even or odd
+
+a = int(input(" Enter a number to check if it's even or odd: "))
+if a<0:
+    print("Enter positive number")
+elif a%2==0:
+    print(a, "is even")
 else:
-    print(b, " is greater than ", a)
-
-
+    print(a, "is odd")
