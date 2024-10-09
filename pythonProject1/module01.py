d146a473ee2629d96c38a5ab8b07addf0f256640
shiv2130 <shivanahuomili@gmail.com>
2024-10-09 12:57:42 +0530

    Pattern printing

--- pythonProject1/module01.py
+++ pythonProject1/module01.py
@@ -29,15 +29,15 @@
 #Printing patterns
 
 
-# n  = int(input("Enter number of rows: "))
-# i = 1
-# while(i<=n):
-#     j = 1
-#     while(j<=i):
-#         print(" * ", end = " ")
-#         j = j + 1
-#     i = i+1
-#     print()
+n  = int(input("Enter number of rows: "))
+i = 1
+while(i<=n):
+    j = 1
+    while(j<=i):
+        print(" * ", end = " ")
+        j = j + 1
+    i = i+1
+    print()
 
 n  = int(input("Enter number of rows: "))
 i = 1
