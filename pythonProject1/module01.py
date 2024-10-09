2d296b8c3533ea59f9501416688900ed0ba83b87
shiv2130 <shivanahuomili@gmail.com>
2024-10-09 12:08:51 +0530

    Factorial

--- pythonProject1/module01.py
+++ pythonProject1/module01.py
@@ -1,7 +1,16 @@
-n = int(input("Enter a number : "))
-s = 0
-i = 0
-while(i<=n):
-    s = s+i
-    i = i+1
-print("sum = ", s)
+# n = int(input("Enter a number : "))
+# s = 0
+# i = 0
+# while(i<=n):
+#     s = s+i
+#     i = i+1
+# print("sum = ", s)
+
+#Program for finding the factorial of n numbers
+
+n = int(input("Enter a number: "))
+f = 1
+while n>0:
+    f = f*n
+    n = n-1
+print(f)
