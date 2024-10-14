458853e6f20803b9801ed1db8c8de7394f2a53b8
shiv2130 <shivanahuomili@gmail.com>
2024-10-14 12:07:48 +0530

    Factorail program in for loop

---
+++ pythonProject1/loops.py
@@ -0,0 +1,16 @@
+# Str1 = "Bhopal"
+# for i in Str1:
+#     print(i, end = " ")
+
+
+
+# for i in range(0, 11):
+#     print(i)
+
+#program for finding out
+n = int(input("Enter a number: "))
+f = 1
+for i in range(1, n+1):
+    f = f*i
+print(f)
+
