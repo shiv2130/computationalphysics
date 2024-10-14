c2bd609d61db749d7cd1df2bde74d7851db0d0d9
shiv2130 <shivanahuomili@gmail.com>
2024-10-14 15:25:25 +0530

    Vector addition

---
+++ vectors.py
@@ -0,0 +1,22 @@
+import numpy as np 
+# list1 = [1, 2, 3]
+# list2 = [[10],
+#          [20],
+#          [30]]
+
+# vector1 = np.array(list1)
+# vector2 = np.array(list2)
+# print("Horizontal vector")
+# print(vector1)
+# print("---------------------")
+# print("vertical vector")
+# print(vector2)
+
+#wrtie a simple program to addition of 2 vector
+b = eval(input("Enter a vector:"))
+c = eval(input("Enter a vector: "))
+v1 = np.array(b)
+v2 = np.array(c)
+print(v1 + v2)
+
+
