3e92098d2cd698eee57a523be194276bc4075170
shiv2130 <shivanahuomili@gmail.com>
2024-10-14 12:56:41 +0530

    Break and Continue

---
+++ pythonProject1/break.py
@@ -0,0 +1,42 @@
+# # Break and continue in python
+# #with break
+# n = int(input("Enter number rows: "))
+# for i in range(0, n+1):
+#     for j in range(0, i+1):
+#         if(i==2):
+#             break
+#         print("*", end = " ")
+#     print()
+#
+# #with continue
+# n = int(input("Enter number rows: "))
+# for i in range(0, n+1):
+#     for j in range(0, i+1):
+#         if(i==2):
+#             continue
+#         print("*", end = " ")
+#     print()
+#
+#
+# n = int(input("Enter number rows: "))
+# for i in range(0, n+1):
+#     for j in range(0, i+1):
+#         if(j==2):
+#             continue
+#         print("*", end = " ")
+#     print()
+#
+#
+# for val in "string":
+#     if val == "i":
+#         continue#->generally skips the statement and then return to the for loop
+#     print(val)
+# print("The end")
+
+n = int(input("Enter a number to find it's prime or not: "))
+for  i in range(2, n):
+    if (n%i==0):
+        print("Not prime Number")
+        break
+else:
+    print("Prime")
