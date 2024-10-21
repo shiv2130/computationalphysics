aef404dd3fcff74b990dbe72c5762fa01751c5ee
shiv2130 <shivanahuomili@gmail.com>
2024-10-21 12:48:03 +0530

    Added 4th option to menu driven program

--- pythonProject1/linear_Search.py
+++ pythonProject1/linear_Search.py
@@ -19,7 +19,27 @@ def bsearch(a, b, l, h):
             return bsearch(a, b, l, mid-1)
     else:
         print("Number not found")
+def ascend(b):
 
+    for i in range(len(b)):
+        for j in range(len(b)-1):
+            if b[j] > b[j+1]:
+                t = b[j]
+                b[j] = b[j+1]
+                b[j+1] = t
+    for i in range(len(b)):
+        print(b[i], end=" ")
+
+def descend(a):
+
+    for i in range(len(a)):
+        for j in range(len(a)-1):
+            if a[j] < a[j+1]:
+                t = a[j]
+                a[j] = a[j+1]
+                a[j+1] = t
+    for i in range(len(a)):
+        print(a[i], end = " ")
 
 # c = [10, 15, 20, 89, 200, 250, 300]
 
