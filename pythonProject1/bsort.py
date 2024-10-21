600bbc74437cf687f42acc37b0170a1b84d250d7
shiv2130 <shivanahuomili@gmail.com>
2024-10-21 12:47:06 +0530

    Bubble sort

--- pythonProject1/bsort.py
+++ pythonProject1/bsort.py
@@ -1,19 +1,21 @@
-a = [5, 10, 8, 100, 51, 20, 49, 35, 42]
-# for i in range(len(a)):
-#     for j in range(len(a)-1):
-#         if a[j] > a[j+1]:
-#             t = a[j]
-#             a[j] = a[j+1]
-#             a[j+1] = t
-# for i in range(len(a)):
-#     print(a[i], end=" ")
-
-
-for i in range(len(a)):
-    for j in range(len(a)-1):
-        if a[j] < a[j+1]:
-            t = a[j]
-            a[j] = a[j+1]
-            a[j+1] = t
-for i in range(len(a)):
-    print(a[i], end = " ")
+# def ascend(b):
+#
+#     for i in range(len(b)):
+#         for j in range(len(b)-1):
+#             if b[j] > b[j+1]:
+#                 t = b[j]
+#                 b[j] = b[j+1]
+#                 b[j+1] = t
+#     for i in range(len(b)):
+#         print(b[i], end=" ")
+#
+# def descend(a):
+#
+#     for i in range(len(a)):
+#         for j in range(len(a)-1):
+#             if a[j] < a[j+1]:
+#                 t = a[j]
+#                 a[j] = a[j+1]
+#                 a[j+1] = t
+#     for i in range(len(a)):
+#         print(a[i], end = " ")
